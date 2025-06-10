b4532f887709f25f4c07732b2b01aea401317894
Moerfelden <96487145+Moerfelden@users.noreply.github.com>
2025-06-10 13:39:25 +0200

    Create nextval_person_id_seq.sh

---
+++ scripts/nextval_person_id_seq.sh
@@ -0,0 +1,5 @@
+# Usage: pass in the DB container ID as the argument
+# Set database configurations
+export CT_DB_USERNAME=ct_admin
+export CT_DB_NAME=geoconnections
+cat ./db/nextval_person_id_seq.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
