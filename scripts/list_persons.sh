4b31e741f1490a033928357db5fef93930b5b662
Moerfelden <96487145+Moerfelden@users.noreply.github.com>
2025-06-11 09:53:42 +0200

    Update and rename nextval_person_id_seq.sh to list_persons.sh

--- scripts/list_persons.sh
+++ scripts/list_persons.sh
@@ -2,4 +2,4 @@
 # Set database configurations
 export CT_DB_USERNAME=ct_admin
 export CT_DB_NAME=geoconnections
-cat ./db/nextval_person_id_seq.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
+cat ./db/list_persons.sql | kubectl exec -i $1 -- bash -c "psql -U $CT_DB_USERNAME -d $CT_DB_NAME"
